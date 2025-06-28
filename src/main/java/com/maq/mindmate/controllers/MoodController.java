bb1f2a034e0643de9e8ce0509e69b357bdc6f326
rath23 <muneer3@amityonline.com>
2025-06-29 00:04:38 +0530

    cleared unimplemented imports

--- src/main/java/com/maq/mindmate/controllers/MoodController.java
+++ src/main/java/com/maq/mindmate/controllers/MoodController.java
@@ -2,18 +2,17 @@ package com.maq.mindmate.controllers;
 
 import com.maq.mindmate.dto.MoodEntryRequest;
 import com.maq.mindmate.models.MoodEntry;
-import com.maq.mindmate.models.User;
+
 import com.maq.mindmate.services.MoodService;
-import com.maq.mindmate.services.UserService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/mood")
--- src/main/java/com/maq/mindmate/controllers/UserController.java
+++ src/main/java/com/maq/mindmate/controllers/UserController.java
@@ -1,12 +1,11 @@
 package com.maq.mindmate.controllers;
 
 import com.maq.mindmate.dto.UserDTO;
-import com.maq.mindmate.models.User;
 import com.maq.mindmate.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
